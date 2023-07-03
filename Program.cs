a8bcd88919279709c804f6dac92904e7a03383cf
WhatIsFun <mohammed110051@gmail.com>
2023-07-03 10:35:26 +0400

    First Example

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace cSharp_Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine(a[5]);
+            //Console.WriteLine();
+        }
+
+        static float divide (float a, float b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
