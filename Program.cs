e286d0cc2190d66d2f1fb927747743dc18c49647
WhatIsFun <mohammed110051@gmail.com>
2023-07-03 10:39:26 +0400

    Second Example

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@
     {
         static void Main(string[] args)
         {
-            int[] a = { 1, 2, 3 };
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.WriteLine(a[i]);
-            }
-            Console.WriteLine(a[5]);
-            //Console.WriteLine();
+            //int[] a = { 1, 2, 3 };
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+            //Console.WriteLine(a[5]);
+            Console.WriteLine(divide(4,2));
+            Console.WriteLine(divide(2, 2));
+            Console.WriteLine(divide(2, 0));
         }
 
         static float divide (float a, float b)
