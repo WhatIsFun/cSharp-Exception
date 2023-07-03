c6ec00a0259e53ad7832060010a997c93c8d84b1
WhatIsFun <mohammed110051@gmail.com>
2023-07-03 11:10:55 +0400

    Divide Exception Example

--- Program.cs
+++ Program.cs
@@ -13,11 +13,23 @@
             Console.WriteLine(divide(4,2));
             Console.WriteLine(divide(2, 2));
             Console.WriteLine(divide(2, 0));
+            Console.WriteLine(divide(3, 6));
         }
 
-        static float divide (float a, float b)
+        static float divide (int a, int b)
         {
-            return a / b;
+            try
+            { // Try block identifies a block of code for which particular exceptions is activated. It is followed by one or more catch blocks.
+                return a / b;
+            }catch (DivideByZeroException e)
+            { // Exception handler at the place in a program where you want to handle the problem.
+                return 0;
+            }catch(Exception e)
+            {
+                return -1;
+            }
+
+            
         }
 
 
