ae10599c9adafaddc72dc8cb0f3c96577cd3dbf0
Mohammed Al Sakiti <mohammed110051@gmail.com>
2023-07-03 22:25:03 +0400

    Complete Task 4

--- Program.cs
+++ Program.cs
@@ -34,30 +34,36 @@ namespace cSharp_Exception
             //Console.WriteLine(divide(2, 0));
             //Console.WriteLine(divide(3, 6));
 
-            //Task 1:
-            Console.WriteLine("&& & Task 1 & &&");
-            Console.WriteLine("Enter first number>>> ");
-            int fNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second number>>> ");
-            int sNumber = Convert.ToInt32(Console.ReadLine());
-            Divide(fNumber, sNumber);
+            ////Task 1:
+            //Console.WriteLine("&& & Task 1 & &&");
+            //Console.WriteLine("Enter first number>>> ");
+            //int fNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second number>>> ");
+            //int sNumber = Convert.ToInt32(Console.ReadLine());
+            //Divide(fNumber, sNumber);
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //Console.WriteLine();
+            ////Task 2:
+            //Console.WriteLine("&& & Task 2 & &&");
+            //Console.WriteLine("Enter your age >>> ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //AgeChecker(age);
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //Console.WriteLine();
+            ////Task 3:
+            //Console.WriteLine("&& & Task 3 & &&");
+            //Console.Write("Enter your file path: ");
+            //string filePath = Console.ReadLine();
+            //FilePath(filePath);
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //Console.WriteLine();
+            //Task 4:
+            Console.WriteLine("&& & Task 4 & &&");
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            NumericValues(input);
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.WriteLine();
-            //Task 2:
-            Console.WriteLine("&& & Task 2 & &&");
-            Console.WriteLine("Enter your age >>> ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            AgeChecker(age);
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine();
-            //Task 3:
-            Console.WriteLine("&& & Task 3 & &&");
-            Console.Write("Enter your file path: ");
-            string filePath = Console.ReadLine();
-            FilePath(filePath);
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            Console.WriteLine();
-
         }
 
         //static float divide (int a, int b)
@@ -136,7 +142,20 @@ namespace cSharp_Exception
                 Console.WriteLine("An error occurred while opening the file: " + ex.Message);
             }
         }
-
+        // Task 4: Create a program that asks the user to enter a series of numeric values.
+        // Keep reading values until the user enters a non-numeric input.
+        // Handle any exceptions that may occur during the input process.
+        static void NumericValues(string input)
+        {
+            try
+            {
+                int number = int.Parse(input);
+                Console.WriteLine("You enterd: " + number);
+            }catch(FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
     }
 
 }
